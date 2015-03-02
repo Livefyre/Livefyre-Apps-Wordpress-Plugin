4f2fef0870a2fe1e91cc2ab06d2bbeaf746fa0b7
Mike Soldner <mike@livefyre.com>
2015-03-02 11:56:20 -0800

    Add sidenotes check as well.

--- livefyre-apps/apps/sidenotes/LFAPPS_Sidenotes.php
+++ livefyre-apps/apps/sidenotes/LFAPPS_Sidenotes.php
@@ -117,7 +117,7 @@ if ( ! class_exists( 'LFAPPS_Sidenotes' ) ) {
             /* Are comments open on this post/page? */
             $comments_open = ( $post->comment_status == 'open' );
 
-            $display = $display_posts || $display_pages;
+            $display = $display_posts || $display_pages || Livefyre_Apps::is_app_enabled('sidenotes');
             $post_type = get_post_type();
             if ( $post_type != 'post' && $post_type != 'page' ) {
 
