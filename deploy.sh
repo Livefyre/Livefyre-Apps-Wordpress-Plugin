5a7bff82479bce5d4bbe5d0b7d9d9cd1948bcb37
Mike Soldner <mikesoldner@Mikes-MacBook-Pro-2.local>
2015-10-27 14:21:44 -0700

    Update deploy script.

--- deploy.sh
+++ deploy.sh
@@ -32,9 +32,9 @@ if ! which svn >/dev/null; then
 fi
 
 # Check version in readme.txt is the same as plugin file after translating both to unix line breaks to work around grep's failure to identify mac line breaks
-NEWVERSION1=`grep "^Stable tag:" $GITPATH/readme.txt | awk -F' ' '{print $NF}'`
+NEWVERSION1=`grep "^Stable tag:" $GITPATH/$PLUGINSLUG/readme.txt | awk -F' ' '{print $NF}'`
 echo "readme.txt version: $NEWVERSION1"
-NEWVERSION2=`grep "^Version:" $GITPATH/$MAINFILE | awk -F' ' '{print $NF}'`
+NEWVERSION2=`grep "^Version:" $GITPATH/$PLUGINSLUG/$MAINFILE | awk -F' ' '{print $NF}'`
 echo "$MAINFILE version: $NEWVERSION2"
 
 if [ "$NEWVERSION1" != "$NEWVERSION2" ]; then echo "Version in readme.txt & $MAINFILE don't match. Exiting...."; exit 1; fi
@@ -49,17 +49,32 @@ if git show-ref --tags --quiet --verify -- "refs/tags/$NEWVERSION1"
 		echo "Git version does not exist. Let's proceed..."
 fi
 
-cd $GITPATH
-echo -e "Enter a commit message for this new version: \c"
-read COMMITMSG
-git commit -am "$COMMITMSG"
+# For sanity reasons
+echo ".........................................."
+echo 
+echo "Parameters:"
+echo "Plugin slug: $PLUGINSLUG"
+echo "Current directory: $CURRENTDIR"
+echo "Main plugin file: $MAINFILE"
+echo "Path to git repo: $GITPATH"
+echo "Path to svn temp dir: $SVNPATH"
+echo "svn url: $SVNURL"
+echo "svn username: $SVNUSER"
+echo
+echo ".........................................."
+echo 
 
-echo "Tagging new version in git"
-git tag -a "$NEWVERSION1" -m "Tagging version $NEWVERSION1"
+#cd $GITPATH
+#echo -e "Enter a commit message for this new version: \c"
+#read COMMITMSG
+#git commit -am "$COMMITMSG"
 
-echo "Pushing latest commit to origin, with tags"
-git push origin master
-git push origin master --tags
+#echo "Tagging new version in git"
+#git tag -a "$NEWVERSION1" -m "Tagging version $NEWVERSION1"
+
+#echo "Pushing latest commit to origin, with tags"
+#git push origin master
+#git push origin master --tags
 
 echo 
 echo "Creating local copy of SVN repo ..."
@@ -69,27 +84,33 @@ echo "Clearing svn repo so we can overwrite it"
 svn rm $SVNPATH/trunk/*
 
 echo "Exporting the HEAD of master from git to the trunk of SVN"
-git checkout-index -a -f --prefix=$SVNPATH/trunk/
+git checkout-index livefyre-apps/* -f --prefix=$SVNPATH/trunk/
 
 echo "Ignoring github specific files and deployment script"
 svn propset svn:ignore "deploy.sh
+install.sh
+livefyre-apps/
+livefyre-wpvip-page.txt
+makfile
 README.md
 .git
 .gitignore" "$SVNPATH/trunk/"
 
 echo "Changing directory to SVN and committing to trunk"
 cd $SVNPATH/trunk/
+
 # Add all new files that are not set to be ignored
-svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2}' | xargs svn add
-svn commit --username=$SVNUSER --password$SVNPASSWORD -m "$COMMITMSG"
+svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2}'
+# | xargs svn add
+# svn commit --username=$SVNUSER --password$SVNPASSWORD -m "$COMMITMSG"
 
-echo "Creating new SVN tag & committing it"
-cd $SVNPATH
-svn copy trunk/ tags/$NEWVERSION1/
-cd $SVNPATH/tags/$NEWVERSION1
-svn commit --username=$SVNUSER -m "Tagging version $NEWVERSION1"
+# echo "Creating new SVN tag & committing it"
+# cd $SVNPATH
+# svn copy trunk/ tags/$NEWVERSION1/
+# cd $SVNPATH/tags/$NEWVERSION1
+# svn commit --username=$SVNUSER -m "Tagging version $NEWVERSION1"
 
-echo "Removing temporary directory $SVNPATH"
-rm -fr $SVNPATH/
+# echo "Removing temporary directory $SVNPATH"
+# rm -fr $SVNPATH/
 
 echo "*** FIN ***"
